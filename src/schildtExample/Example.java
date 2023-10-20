7531644f2e3977f75ef99dfab53cc39d93dd90c3
LenFill <lenfill66rus@gmail.com>
2023-10-20 20:29:20 +0500

    1

---
+++ src/schildtExample/Example.java
@@ -0,0 +1,12 @@
+package schildtExample;
+/*
+Простая программа на java.
+Назовите этот файл Example.java.
+ */
+
+public class Example {
+    public static void main(String[] args){
+        System.out.println("Простая программа на языке Java.");
+
+    }
+}
---
+++ src/schildtExample/Example2.java
@@ -0,0 +1,11 @@
+package schildtExample;
+
+public class Example2 {
+    public static void main(String[] args){
+        int num; // Объявление переменной по имени num
+        num = 100; // Присваивание(Инициализация) переменной num значения 100
+        System.out.println("Значение num: " + num);
+        num = num * 2;
+        Syst
+    }
+}
