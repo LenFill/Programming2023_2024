c510850fc6a7cf14b47f1bb881408b8adb8a9e72
LenFill <LenFill66rus@gmail.com>
2024-01-02 21:58:53 +0500

    02.01.2024

--- src/exampleLab5/example_1/Example_1.java
+++ src/exampleLab5/example_1/Example_1.java
@@ -1,4 +1,4 @@
-package exampleLab5;
+package exampleLab5.example_1;
 /*
 Напишите программу с классом, в котором есть закрытое символьное поле
 и три открытых метода. Один из методов позволяет присвоить значение полю.
@@ -6,4 +6,19 @@ package exampleLab5;
 метод позволяет вывести в консольное окно символ (значение поля) и его код.
 */
 public class Example_1 {
+    private char sim1;
+
+    public void setSim1(char sim1) {
+        this.sim1 = sim1;
+    }
+
+    public int getCode(){
+        return  sim1;
+    }
+
+    public void showSim1AndCode() {
+        System.out.print(sim1 + " ");
+        System.out.print((int) sim1);
+    }
 }
+
---
+++ src/exampleLab5/example_1/Main.java
@@ -0,0 +1,11 @@
+package exampleLab5.example_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Example_1 exe = new Example_1();
+        exe.setSim1('e');
+        System.out.println(exe.getCode());
+        exe.showSim1AndCode();
+
+    }
+}
